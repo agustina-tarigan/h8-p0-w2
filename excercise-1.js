c15526fbca16f18bea1c74ec1ec9f5e41a263677
unknown <thinatarigan@gmail.com>
2017-12-19 08:14:42 +0700

    nambah readme dan exercise-1

---
+++ excercise-1.js
@@ -0,0 +1,11 @@
+var number1 = 33, number2 = 6;
+console.log(number1);
+console.log(number2);
+
+var string1 = '"foo"', string2 = '"bar"';
+console.log(string1);
+console.log(string2);
+
+var boolean1 = true, boolean2 = false;
+console.log(boolean1);
+console.log(boolean2);
