e212545c44c0a9c9120b36cc7356cfa40365e24e
unknown <thinatarigan@gmail.com>
2017-12-20 05:54:36 +0700

    add exercise 5

---
+++ exercise-5.js
@@ -0,0 +1,14 @@
+console.log('LOOPING PERTAMA');
+
+for(var deret = 1; deret < 21; deret++)
+{
+  console.log(deret + ' - I love coding');
+}
+
+
+console.log('LOOPING KEDUA');
+
+for(var deret = 20; deret > 0; deret--)
+{
+  console.log(deret + ' - I will become fullstack developer');
+}
