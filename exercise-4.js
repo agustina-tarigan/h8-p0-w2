6f841449bd04068ad32f9ebcf0adde86c5c589c7
unknown <thinatarigan@gmail.com>
2017-12-20 05:46:59 +0700

    add exercise 4

---
+++ exercise-4.js
@@ -0,0 +1,16 @@
+console.log('LOOPING PERTAMA.')
+var flag = 2;
+while
+  (flag < 22) // loop akan terus berjalan selama nilai flag masih dibawah 22
+  {
+    console.log(flag+ ' - i love coding'); //menampilkan nilai flag pada iterasi tertentu
+   flag+=2; //mengubah nilai flag dengan menambahkan 2
+  }
+console.log('LOOPING KEDUA.')
+var flag = 20;
+while
+  (flag > 0) // loop akan terus berjalan selama nilai flag masih diatas 0
+  {
+    console.log(flag+ ' - i will become fullstack developer');
+    flag-=2;
+  }
