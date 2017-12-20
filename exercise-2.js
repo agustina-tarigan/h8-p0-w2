7ff4257e63ab668a7f49573e63c8bd4dd1bc3d4c
unknown <thinatarigan@gmail.com>
2017-12-20 14:41:41 +0700

    add exercise 2

---
+++ exercise-2.js
@@ -0,0 +1,16 @@
+var nama = 'Mikael'
+var peran = ''
+
+if (peran == 'Ksatria')
+{
+	console.log (`Selamat datang di Dunia Proxytia, ${nama}. Halo ${peran} ${nama}, kamu dapat menyerang dengan senjatamu!`);
+} else if (peran == 'Tabib')
+{
+	console.log (`Selamat datang di Dunia Proxytia, ${nama}. Halo ${peran} ${nama}, kamu akan membantu temanmu yang terluka.`);
+} else if (peran == 'Penyihir')
+{
+	console.log	(`Selamat datang di Dunia Proxytia, ${nama}. Halo ${peran} ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
+} else
+{
+	console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`);
+}
