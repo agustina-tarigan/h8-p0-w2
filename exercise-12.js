33dad2bcdc82822a8506da19b5f4ce65fd75c0fa
unknown <thinatarigan@gmail.com>
2017-12-26 06:48:42 +0700

    Add exercise 12

---
+++ exercise-12.js
@@ -0,0 +1,10 @@
+var kata = "Hello World!"
+
+function balikString(kata)
+
+ {
+ for (var balikString=kata.length - 1; balikString >= 0; balikString--);
+ }
+console.log(balikString[kata])
+
+// output nya undefined, saya ga ngerti kenapa bisa undefined
